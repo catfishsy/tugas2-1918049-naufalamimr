20d0189055192fd6d7968105d00fa666f6325ede
amima <ninja_cat712@yahoo.com>
2021-10-11 15:42:33 +0700

    2 fragmennn bro

---
+++ app/src/main/java/com/raywenderlich/pertemuan_2_1918049/MainActivity.java
@@ -0,0 +1,14 @@
+package com.raywenderlich.pertemuan_2_1918049;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
